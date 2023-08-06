657ae1901317233fb29c630ffb6e00e9ca5d8841
Clayton Prickett <71116858+clayandthepotter@users.noreply.github.com>
2023-08-05 21:07:23 -0500

    add limerick

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,2 +1,8 @@
 // This is my first program!
 console.log('Hello World!');
+
+console.log("There once was a writer of code");
+console.log('Who wanted to learn JS and node');
+console.log('They said "Once I can learn,"');
+console.log("'I'll increase what I earn.'");
+console.log("So now they're in full programmer mode!");
