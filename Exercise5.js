17202fdce8aa1e339286fcadc174452a01fedb7e
Clayton Prickett <71116858+clayandthepotter@users.noreply.github.com>
2023-08-06 09:23:09 -0500

    Create Exercises

---
+++ Exercise5.js
@@ -0,0 +1,13 @@
+// Ex. 5
+
+console.log("True or false: 5 is less than 2");
+console.log(5 < 2); // false
+console.log("True or false: 19 is not equal 11");
+console.log(19 != 11) // true
+console.log("True or false: 10 is greater than or equal to 10");
+console.log(10 >= 10); // true
+
+console.log(2 == 2); // ==, true
+console.log(10 > 11); // >, false
+console.log(11 <= 10); // <=, false
+console.log(7 != 5); // !=, true
