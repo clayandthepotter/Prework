5a181383957e870a764e089b7c3df116454ef80a
Clayton Prickett <71116858+clayandthepotter@users.noreply.github.com>
2023-08-06 11:23:52 -0500

    Create Exercise13.js

---
+++ Exercise13.js
@@ -0,0 +1,10 @@
+// Ex. 13
+
+const x = 10;
+
+if (x < 100) {
+    console.log("The condition was true!");
+    console.log(`${x} is less than 100`);
+}
+
+console.log("This will be displayed regardless.");
