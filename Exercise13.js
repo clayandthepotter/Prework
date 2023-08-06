f86e1ddf74ffd5cf60fd93591873b0e8388e2555
Clayton Prickett <71116858+clayandthepotter@users.noreply.github.com>
2023-08-06 12:38:49 -0500

    Update Exercise13.js

--- Exercise13.js
+++ Exercise13.js
@@ -8,3 +8,55 @@ if (x < 100) {
 }
 
 console.log("This will be displayed regardless.");
+
+//---
+
+const myPassword = "abc123";
+
+if (myPassword.length < 10) {
+    console.log("Sorry this password isn't long enough, try again.");
+} else {
+    console.log("Great password length!");
+}
+
+//---
+
+let day = "Tuesday";
+let special = ""; //you don't know what the special will be yet so make it an empty string for now
+
+if (day == "Monday") {
+    special = "blueberry muffin";
+} else if (day == "Tuesday") {
+    special = "coffee cake";
+} else if (day == "Wednesday") {
+    special = "chocolate croissant";
+} else if (day == "Thursday") {
+    special = "lemon bar";
+} else if (day == "Friday") {
+    special = "cupcake";
+} else {
+    console.log(`Today's ${day}!`);
+    console.log("Sorry we're closed on the weekend!");
+}
+
+
+//if the length of special is not greater than 0 it means it’s still empty and thus it must be a weekend
+if (special.length > 0) {
+    console.log(`Today is ${day} and the special is ${special}.`);
+}
+
+
+//---
+
+
+/*Fill in the provided code template to let someone know which movie ratings they are able to see based on their age. People aged 17 and up can see any movie; people aged 13-16 can see PG-13, PG, or G movies, and everyone else can only see PG or G movies. */
+
+const age = 29;
+
+if (age >= 17) {
+    console.log(`You are ${age}. You are old enough to see any movie: R, PG-13, PG, or G.`);
+} else if (age >= 13) {
+    console.log(`You are ${age}. You are only old enough to see PG-13, PG, and G movies.`)
+} else {
+    console.log(`You are ${age}. You are only old enough to see PG and G movies.`);
+}
