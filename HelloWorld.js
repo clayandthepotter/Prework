fa4c4818dffbefec60a80c5c29a8bf2d0f390750
Clayton Prickett <71116858+clayandthepotter@users.noreply.github.com>
2023-08-05 23:19:13 -0500

    Add Ex. 6

--- HelloWorld.js
+++ HelloWorld.js
@@ -75,3 +75,30 @@ console.log(2 == 2); // ==, true
 console.log(10 > 11); // >, false
 console.log(11 <= 10); // <=, false
 console.log(7 != 5); // !=, true
+
+// Ex. 6
+
+console.log("true or false: wrapping in double quotes is the same as wrapping in single quotes");
+console.log("Hello world!" == 'Hello world!');
+
+console.log("true or false: using single quotes inside a string is the same as using double quotes inside the string");
+console.log("He said 'no'!" == 'He said "no"!');
+
+console.log("true or false: strings that say the same thing but have different capitalization are not equal");
+console.log("Hello world!" != "hello world!");
+
+console.log("true or false: in javascript, a string is considered less than another string if it would come before it in alphabetical order");
+console.log("apple" < "banana");
+
+console.log("The number 5 is the same value as the string '5'");
+console.log(5 == "5");
+
+console.log("The number 5 is the same value and same data type as the string '5'");
+console.log(5 === "5");
+
+
+console.log('5' == "5"); // ==, true
+console.log(1 < "-1"); // <, false
+console.log('hello' !== 'hello'); // !==, false
+console.log(5 != "5.1"); // !=, true
+console.log("5" === '5'); // ===, true
